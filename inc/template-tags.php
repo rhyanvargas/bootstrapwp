c33948f4a6eb59d89b84f5a43f06c7d893b40f25
Rhyan Vargas <rhyan@rhyanvargas.com>
2017-12-14 14:12:09 -0500

    add styles to entry meta section in posts

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -81,13 +81,13 @@ if ( ! function_exists( 'bootstrapsasswp_posted_on' ) ) :
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'bootstrapsasswp' ),
+			_x( '<i class="fas fa-clock"></i> %s', 'post date', 'bootstrapsasswp' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
 		$byline = sprintf(
 			/* translators: %s: post author. */
-			esc_html_x( 'by %s', 'post author', 'bootstrapsasswp' ),
+			_x( '<i class="fas fa-user"></i> %s', 'post author', 'bootstrapsasswp' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
